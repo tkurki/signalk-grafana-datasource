b142e96a0ad9e23b04f89fb0ecbd9ad341b8c153
Teppo Kurki <teppo.kurki@iki.fi>
2026-02-13 23:49:56 +0200

    adjust for sk new install location

--- docker-compose-config/dotsignalk/start.sh
+++ docker-compose-config/dotsignalk/start.sh
@@ -4,4 +4,4 @@ if [ ! -d "node_modules/signalk-to-influxdb2" ]; then
     npm install --save signalk-to-influxdb2
 fi
 
-/usr/lib/node_modules/signalk-server/bin/signalk-server
+/home/node/signalk/node_modules/signalk-server/bin/signalk-server
